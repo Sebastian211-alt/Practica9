501294c3c2efbf7e00e54cc6c1199abfb7ec543b
Sebastil24kc <85630110+Sebastian211-alt@users.noreply.github.com>
2022-03-30 08:12:18 -0600

    Update Clase9.java

--- src/main/java/Practica9/Clase9.java
+++ src/main/java/Practica9/Clase9.java
@@ -1,3 +1,32 @@
+/*
+    Realiza un simulador de máquina tragaperras simplificada que cumpla los
+siguientes requisitos:
+
+a) El ordenador mostrará una tirada que consiste en mostrar 3 figuras. Hay 5
+figuras posibles: corazón, diamante, herradura, campana y limón.
+
+b) Si las tres figuras son diferentes se debe mostrar el mensaje “Lo siento, ha
+perdido”.
+
+c) Si hay dos figuras iguales y una diferente se debe mostrar el mensaje “Bien,
+ha recuperado su moneda”.
+
+d) Si las tres figuras son iguales se debe mostrar “Enhorabuena, ha ganado
+10 monedas”.
+
+Ejemplo 1:
+diamante diamante limón
+Bien, ha recuperado su moneda
+
+Ejemplo 2:
+herradura campana diamante
+Lo siento, ha perdido
+
+Ejemplo 3:
+corazón corazón corazón
+Enhorabuena, ha ganado 10 monedas
+
+*/
 
 package Practica9;
 
